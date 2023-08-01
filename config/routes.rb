044cb659ec64e3b8c83839bc3855aa51783a40d1
trofimovmax <92295615+TrofimovMax@users.noreply.github.com>
2023-08-01 13:42:26 +0300

    feat: create many tot many association - courses_category

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :courses_categories
+  resources :categories
+  resources :courses
+  #resources :courses_сategories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  #root 'categories#index'
 end
